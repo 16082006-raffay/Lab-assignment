fca8e22ec68675bfa4a93ecdd2fa6ca6cf8db1b8
16082006-raffay <raffay1608@gmail.com>
2025-09-09 23:56:57 +0500

    Add files via upload

---
+++ Class Projects.py
@@ -0,0 +1,9 @@
+exam_score = 79
+if exam_score == 85:
+    print("exelent")
+elif exam_score >70  <84 :
+    print("good work!")
+elif exam_score >50 <69:
+    print("you passed")
+else:
+    print("you fail")
